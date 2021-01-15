a68b9e979064728c244461f46fdcbd867867ff18
STEIN <doruchenko.m.i@gmail.com>
2021-01-15 12:15:35 +0200

    Updates

---
+++ precache-manifest.22484e3f3268f9b976ce2c92326fe26b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f52c6a041ed30aa95e76f1c6daaf007c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fec55b3def4d6b67dc3",
+    "url": "./static/css/main.a046dbad.chunk.css"
+  },
+  {
+    "revision": "0ec6addaeb8d40ac9c8e",
+    "url": "./static/js/2.ac3f5f51.chunk.js"
+  },
+  {
+    "revision": "3fec55b3def4d6b67dc3",
+    "url": "./static/js/main.8ab7f4a7.chunk.js"
+  },
+  {
+    "revision": "fae9b0ee980da1f71458",
+    "url": "./static/js/runtime-main.bc4bc2f5.js"
+  }
+]);
